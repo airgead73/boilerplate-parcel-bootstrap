4265bb2aeb281af844c36f025a265d96260a23e6
Dan Moneypenny <catullus1973@gmail.com>
2022-01-22 09:10:30 -0800

    add bootswatch

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,6 @@
 // Import bootstrap plugins
-import * as bootstrap from 'bootstrap';
+//import * as bootstrap from 'bootstrap';
 
-const fetchAuthors = async () => {
-  const response = await fetch("/authors");
-  const data = await response.json();
-  console.log("authors", data);
-}
+import { Modal as Modal } from 'bootstrap';
 
-const fetchBooks = async () => {
-  const response = await fetch("/books");
-  const data = await response.json();
-  console.log("books", data);
-}
-
-fetchAuthors();
-fetchBooks();
+console.log('app has loaded')
